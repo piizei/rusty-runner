11f1a6abf43fc2c3eb946f8b98f47f5204b76907
Petteri Johansson <191493+piizei@users.noreply.github.com>
2022-11-04 13:41:23 +0000

    dev container updates

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,6 +22,15 @@ rustup component add clippy --toolchain nightly
 cargo install cargo-expand
 cargo install cargo-edit
 
+#NPM
+# set-up and install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install yarn -y
+
+# instsal nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
 ## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
